55c44f8b451e28a65dc2826d95e6004e0235c97d
unknown <tella4164@gmail.com>
2024-11-23 15:38:29 +0900

    fix Lv3

--- ch2_kiosk/src/Lv3/Main.java
+++ ch2_kiosk/src/Lv3/Main.java
@@ -1,16 +1,12 @@
 package Lv3;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Kiosk kiosk = new Kiosk();
 
-        kiosk.startKiosk();
+        kiosk.start();
 
     }
 }
