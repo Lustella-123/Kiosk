26c22508e5c50841319249ced9220a17ad17c675
unknown <tella4164@gmail.com>
2024-11-25 10:43:19 +0900

    :sparkles:upload challenge_Lv1

---
+++ ch2_kiosk/src/challenge_Lv1/MenuItem.java
@@ -0,0 +1,27 @@
+package challenge_Lv1;
+
+public class MenuItem {
+    private String name;
+    private double price;
+    private String description;
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    // Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+}
